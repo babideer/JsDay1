5ed25e9686ff55cc49f4c9d0b4abcf2c7a148f6e
babideer <55578454+babideer@users.noreply.github.com>
2021-09-01 11:49:48 +0200

    Add files via upload
    finished exercises

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,8 @@
+
+
 /*exercise 1*/
+console.log('Exercise 1');
+
     let base = 7;
     let height = 4;
 
@@ -7,6 +11,7 @@
     console.log(areal);
 
 /*exercise 2*/
+console.log('Exercise 2');
     const name = 'Amanda';
     let lastN = 'Blondal';
     let salary = 5000;
@@ -17,11 +22,13 @@
     console.log(name,lastN + ' salary before tax is ' + salary + ' .She pays ' + salary * 0.25 + ' in taxes.');
 
 /*exercise 3*/
+console.log('Exercise 3');
     let sentence = 'hello, my name is Amanda';
     console.log(sentence.length); //length of string
     console.log(sentence.indexOf('Amanda')); //name placement
 
 /*exercise 4*/
+console.log('Exercise 4');
     const capName = 'amanda';
     const str1 = capName.slice(1); //remove first letter
     console.log(str1);
@@ -30,20 +37,96 @@
     console.log(str2);
 
 /*exercise 5*/
-    
+console.log('Exercise 5');
+    let accountN = 4000;
+    console.log(accountN);
+
+    accountN++; //add 1 
+    console.log(accountN);
+    accountN++;
+    accountN++;
+    accountN++;
+    accountN++; //adds 4
+    console.log(accountN); 
+
+    accountN--; //subtracts 1
+    console.log(accountN); 
+
+    accountN+=5; //adds 5
+    console.log(accountN);
+
+    accountN -=10; //subtracts 10
+    console.log(accountN);
 
 /*exercise 6*/
-    const classN = ['Jeppe','Sara','Carolina','Josefine','Amanda','Lotte'];
+console.log('Exercise 6');
+    let classN = ['Jeppe','Sara','Carolina','Josefine','Amanda','Lotte'];
     console.log(classN); 
 
-    let newLength = classN.push('Mie'); //add classmate
+    classN.push('Mie');//add a classmate
     console.log(classN);
 
-    /*remove index 3*/
-
-    let last = classN.pop(); //remove last classmate
+    classN[3] = 'Imad';//index 3 placement
     console.log(classN);
 
-    /*show number of indexes*/
+    classN.pop();//remove last classmate
+    console.log(classN);
+    
+    console.log(classN.length);//show number of indexes
+    
+    console.log(classN.includes('Amanda'));//show own name
 
-    /*show own name*/
+/*exercise 6.1*/
+console.log('Exercise 6.1');
+    const daneA = ['Mads Mikkelsen', 'Ove Sprogøe', 'Dirch Passer'];
+    const americanA = ['Leonardo Dicaprio', 'Johnny Depp', 'Helena Carter'];
+
+    console.log(daneA, americanA);
+
+    const conA = daneA.concat(americanA);
+    console.log(conA);
+
+/*exercise 6.2*/
+console.log('Exercise 6.2');
+    console.log(conA.indexOf('Leonardo Dicaprio')); //find position
+
+    conA[3] = 'Leonardo Wilhelm Dicaprio'  //replace Leonardo
+    console.log(conA);
+
+    console.log(conA.join(', '));
+
+/*exercise 7*/
+console.log('Exercise 7');
+
+    const myAge = 24;
+    const sideJep = 22;
+
+   console.log(myAge == sideJep) //same age
+
+   console.log(myAge != sideJep)//age is different (! difference)
+
+   console.log(myAge < sideJep)//one age is greater than og equal to
+    
+   console.log(myAge > sideJep)//one age is less than or equal to
+
+/*exercise 8*/
+console.log('Exercise 8'); 
+    let mysAge = '24';
+    console.log(typeof mysAge); //show variable is a string
+
+
+``
+/*exercise 9*/
+console.log('Exercise 9');
+
+    let name2 = `Amanda`;
+    let lastName2 = `Blondal`;
+    let myAge2 = `24`;
+    let favourShow = `Modern Family`;
+    let favourMovie = `Alien`;
+
+console.log(`My name is ${name} ${lastName2}. I am ${myAge2} years old.  My favourite movie is ${favourMovie}`); //presentation with all variables in a string
+    
+
+
+
